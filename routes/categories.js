5de5b9e8d1b21fca7498e6690989f7fb93497bff
Macbook Air <macbookair@David-Ramirez.netgear.com>
2017-03-27 22:51:33 -0700

    update the backend routes

--- routes/categories.js
+++ routes/categories.js
@@ -1,26 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-var Category =  require('../models/category');
 
-//GET All Articles
+var Category = require('../models/category');
+
+/* GET categories */
 router.get('/', function(req, res, next) {
   Category.getCategories(function(err, categories){
-    if(err){
-      console.log(err);
-    }
-    res.json(categories)
-  });
+  	if(err){
+  		console.log(err);
+  	}
+  	res.json(categories);
+  })
 });
 
-//GET Artlcle Show
 router.get('/:id', function(req, res, next) {
-  Article.getCategoryById(req.params.id, function(err, category){
-    if(err){
-      console.log(err);
-    }
-    res.json(category)
-  });
+  Category.getCategoryById(req.params.id, function(err, category){
+  	if(err){
+  		console.log(err);
+  	}
+  	res.json(category);
+  })
+});
+
+router.get('/category/:category', function(req, res, next) {
+  Article.getArticleByCategory(req.params.category, function(err, article){
+  	if(err){
+  		console.log(err);
+  	}
+  	res.json(article);
+  })
 });
 
 
